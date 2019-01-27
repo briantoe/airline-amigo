4671292ce2ebf645b360f518b0a7befe860cf00b
James Dunlap <masterjd777@hotmail.com>
2019-01-27 10:37:57 -0500

    added bathroom

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,37 +4,14 @@ import { Link } from 'react-router-dom';
 import './Home.css';
 
 export default class Home extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      occupiedBathroom: true // default is UN-occupied
-    };
-  }
-
   render() {
 
-    // if(this.state.occupiedBathroom)
-    // {
-    //   this.toggleColor();
-    // }
     return (
-      <div className='parent'>
-        <div color='green' id='bathroom' className='bathroomOpen'>
-          <p color='green'>Restroom Status</p>
-        </div>
-        <div className='Links'>
-          <Link to='/translate'>Tanslate</Link>
-          <Link to='/flight'>Flight</Link>
-          <Link to='/movies'>Movies</Link>
-        </div>
-        <button onClick={this.toggleColor}>.</button>
+      <div className='Links'>
+        <Link to='/translate'>Tanslate</Link>
+        <Link to='/flight'>Flight</Link>
+        <Link to='/movies'>Movies</Link>
       </div>
     );
-  }
-
-  toggleColor = () => {
-    var element = document.getElementById('bathroom');
-    element.classList.toggle("bathroomClosed");
   }
 }
