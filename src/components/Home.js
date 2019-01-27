f6965542d0daf284e452336d3c7435265726b868
James Dunlap <masterjd777@hotmail.com>
2019-01-27 08:26:32 -0500

    added toggle button to toggle restroom status

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,45 @@ import { Link } from 'react-router-dom';
 import './Home.css';
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+     occupiedBathroom: true // default is UN-occupied
+    };
+
+
+  
+
+  }
+
   render() {
+
+    // if(this.state.occupiedBathroom)
+    // {
+    //   this.toggleColor();
+    // }
     return (
-      <div className='Links'>
-        <Link to='/translate'>Tanslate</Link>
-        <Link to='/flight'>Flight</Link>
-        <Link to='/movies'>Movies</Link>
+      <div className='parent'>
+        <div color='green' id='bathroom' className='bathroomOpen'>
+          <p color='green'>Restroom Status</p>
+        </div>
+        <div className='Links'>
+          <Link to='/translate'>Tanslate</Link>
+          <Link to='/flight'>Flight</Link>
+          <Link to='/movies'>Movies</Link>
+        </div>
+        
+   <button onClick={this.toggleColor}>.</button>
+
       </div>
+
     );
   }
+
+  toggleColor = () => {
+    var element = document.getElementById('bathroom');
+    element.classList.toggle("bathroomClosed");
+  }
+
 }
