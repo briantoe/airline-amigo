d85c0f2b6e3fd320caf38277a897c33083e715db
briantoe <bht170030@utdallas.edu>
2019-01-27 05:18:39 -0600

    completed translation component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
-import io from 'socket.io-client';
 
-import langs from '../languages';
-
-// Component Imports
-import Users from './Users';
-import Video from './Video';
-import Chat from './Chat';
-import Call from './Call';
-import {flight} from './flight';
-
-// CSS Import
 import './App.css';
+import langs from './languages';
+
+const axios = require('axios');
+
 
 class App extends Component {
 
@@ -19,11 +12,26 @@ class App extends Component {
     super(props);
 
     this.state = {
-      lang: 'en',
-      username: null
+      recording: false,
+      // passengerSubtitles: [],
+      // attendantSubtitles: [],
+      subtitles: [],
+      interimTranscript: '',
+      value: true,
+      lang: 'en-US', // english is default language in the list
+      speaker: ''
     };
 
-    this.socket = io(process.env.REACT_APP_SOCKET_SERVER);
+
+    // Speech
+    this.recognition = new window.webkitSpeechRecognition();
+    this.recognition.interimResults = true;
+    this.recognition.onresult = this.onTranscript;
+
+    // Synthesis
+    this.synth = window.speechSynthesis;
+
+
   }
 
   render() {
@@ -31,60 +39,46 @@ class App extends Component {
       <option value={lang.code} key={lang.code}>{lang.name}</option>
     ));
 
-    if (!this.state.username) {
-      console.log('rendering screen');
-      return (
-        <div className='App'>
-          <select value={this.state.lang} onChange={this.handleChange}>
-            {langList}
-          </select>
-          <div className='Splash'>
-            <div className='Title'>BABEL TALK</div>
-            <div className='Subtitle'>The real-time video calling voice-to-voice translation app!</div>
-            <input type='text' onKeyPress={this.onKeyPress} ref={el => { this.input = el }} placeholder="What's your name?" />
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <u>Translation application</u>
+          <div>
+            <button disabled={!this.state.value} onMouseUp={this.stopRecording} onMouseDown={this.record} name='attendant' className='button button-left'>
+              <b>
+                Attendant
+           </b>
+            </button>
+
+
+            <button disabled={!this.state.value} onMouseUp={this.stopRecording} onMouseDown={this.record} name='passenger' className='button button-right'>
+              <b>
+                Passenger
+             </b>
+            </button>
           </div>
-        </div>
-      );
-    }
-    else {
-      return (
-        <div className='App'>
-          <select value={this.state.lang} onChange={this.handleChange}>
-            {langList}
-          </select>
-          <Call socket={this.socket} />
-          <Users username={this.state.username} socket={this.socket} />
-          <div className='VideoChat'>
-            <Video lang={this.state.lang} socket={this.socket} />
-            <Chat lang={this.state.lang} socket={this.socket} />
+
+          <div>
+            <a>Language: </a>
+            <select value={this.state.lang} onChange={this.handleChange}>
+              {langList}
+            </select>
+
           </div>
-        </div>
-      );
-    }
+
+
+        </header>
+      </div >
+    );
+
   }
 
-  onKeyPress = (event) => {
-    if (event.key === 'Enter') {
-      let text = this.input.value;
-      this.setState({
-        username: text
-      });
-      this.input.value = null;
-      this.socket.emit('join', { id: this.socket.id, username: text, lang: this.state.lang });
-    }
-  }
 
-  componentDidMount() {
-    flight(8921,"2018-07-07", (res) => {});
-    let localLang = navigator.language;
-    langs.forEach(lang => {
-      if (lang.code === localLang) {
-        this.setState({
-          lang: localLang
-        });
-      }
-    });
-  }
+
+  // funcs
+
+
 
   handleChange = (event) => {
     console.log("lang change event");
@@ -92,9 +86,137 @@ class App extends Component {
     this.setState({
       lang: event.target.value
     });
-    // this.socket.emit(tell the server that language has changed)
-    this.socket.emit('lang', { lang: event.target.value });
+
   }
+
+
+  tempDisable = () => {
+    this.setState({
+      value: !this.state.value
+    });
+    console.log("buttons disabled.");
+    // set time out    
+    setTimeout(() => {
+      this.setState({
+        value: !this.state.value
+      });
+    }, 2000);
+
+  }
+
+  // Toggle muting mic
+  record = (event) => {
+    this.recognition.start();
+    console.log("recording");
+
+    this.setState({
+      recording: true
+
+    });
+    let buttonPressed = event.target.innerText;
+
+    if (buttonPressed === "Passenger") {
+      console.log("passenger pressed");
+      this.setState({
+        speaker: this.state.lang
+      });
+    }
+    else if (buttonPressed === "Attendant") {
+      console.log('attendant pressed');
+      this.setState({
+        speaker: 'en-US'
+      });
+    }
+
+  }
+ 
+  stopRecording = () => {
+    this.recognition.stop();
+    this.setState({
+      recording: false
+    });
+    this.tempDisable();
+
+  }
+
+  receivedTranslation = (transcript) => {
+    console.log('RECEIVED TRANSCRIPT:', transcript);
+
+    this.setState({
+      subtitles: this.state.subtitles.concat(transcript)
+    });
+    let text = transcript[0].translatedText;
+
+    let utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = this.state.lang;
+    this.synth.speak(utterance);
+  }
+
+  onTranscript = (event) => {
+    console.log("on transcript");
+    console.log(event.results);
+    let results = event.results;
+    this.setState({
+      interimTranscript: ''
+    })
+    for (let i = event.resultIndex; i < results.length; ++i) {
+      let transcript = results[i][0].transcript;
+
+      if (results[i].isFinal) {
+        console.log(transcript);
+
+        if (this.state.speaker === this.state.lang) // you're not even translating into another language :thinking:
+          return;
+
+        if (this.state.speaker === 'en-US') {
+          let truncated = this.state.lang;
+          truncated = truncated.substring(0,2);
+          translate(transcript, truncated, 'en', this.receivedTranslation);
+        }
+        else {
+          let truncated = this.state.speaker;
+          truncated = truncated.substring(0,2)
+          translate(transcript, 'en', truncated, this.receivedTranslation);
+        }        // setTimeout(() => {
+        //   console.log('timeout');
+        //   let transcripts = this.state.subtitles;
+        //   for (let i = 0; i < transcripts.length; i++) {
+        //     if (transcripts[i] === transcript) {
+        //       transcripts.splice(i, 1);
+        //     }
+        //   }
+
+
+        //   this.setState({
+        //     subtitles: transcripts
+        //   });
+        // }, 5000);
+
+      } else {
+        this.setState({
+          interimTranscript: this.state.interimTranscript + transcript + ' '
+        });
+      }
+    }
+  }
+
+}
+
+const translate = (text, targetLang, sourceLang, callback) => {
+  let token = process.env.TRANSLATION_API_KEY;
+  
+  axios.post('https://translation.googleapis.com/language/translate/v2?key=' + token, {
+    q: text,
+    target: targetLang,
+    source: sourceLang
+  }).then(res => {
+    console.log(res.data.data.translations);
+    callback(res.data.data.translations)
+  })
+    .catch(err => {
+      console.log(err.response.data);
+      callback(null, err)
+    });
 }
 
 export default App;
---
+++ client/src/components/languages.js
@@ -0,0 +1,189 @@
+
+const langs = [
+
+{ 
+ 'code': 'ar-SA', 
+'name': 'Arabic-Saudi Arabia' 
+ },
+
+{ 
+ 'code': 'cs-CZ', 
+'name': 'Czech-Czech Republic' 
+ },
+
+{ 
+ 'code': 'da-DK', 
+'name': 'Danish-Denmark' 
+ },
+
+{ 
+ 'code': 'de-DE', 
+'name': 'German-Germany' 
+ },
+
+{ 
+ 'code': 'el-GR', 
+'name': 'Modern-Greek        Greece' 
+ },
+
+{ 
+ 'code': 'en-AU', 
+'name': 'English-Australia' 
+ },
+
+{ 
+ 'code': 'en-GB', 
+'name': 'English-United Kingdom' 
+ },
+
+{ 
+ 'code': 'en-IE', 
+'name': 'English-Ireland' 
+ },
+
+{ 
+ 'code': 'en-US', 
+'name': 'English-United States' 
+ },
+
+{ 
+ 'code': 'en-ZA', 
+'name': 'English-South Africa' 
+ },
+
+{ 
+ 'code': 'es-ES', 
+'name': 'Spanish-Spain' 
+ },
+
+{ 
+ 'code': 'es-MX', 
+'name': 'Spanish-Mexico' 
+ },
+
+{ 
+ 'code': 'fi-FI', 
+'name': 'Finnish-Finland' 
+ },
+
+{ 
+ 'code': 'fr-CA', 
+'name': 'French-Canada' 
+ },
+
+{ 
+ 'code': 'fr-FR', 
+'name': 'French-France' 
+ },
+
+{ 
+ 'code': 'he-IL', 
+'name': 'Hebrew-Israel' 
+ },
+
+{ 
+ 'code': 'hi-IN', 
+'name': 'Hindi-India' 
+ },
+
+{ 
+ 'code': 'hu-HU', 
+'name': 'Hungarian-Hungary' 
+ },
+
+{ 
+ 'code': 'id-ID', 
+'name': 'Indonesian-Indonesia' 
+ },
+
+{ 
+ 'code': 'it-IT', 
+'name': 'Italian-Italy' 
+ },
+
+{ 
+ 'code': 'ja-JP', 
+'name': 'Japanese-Japan' 
+ },
+
+{ 
+ 'code': 'ko-KR', 
+'name': 'Korean-Republic of Korea' 
+ },
+
+{ 
+ 'code': 'nl-BE', 
+'name': 'Dutch-Belgium' 
+ },
+
+{ 
+ 'code': 'nl-NL', 
+'name': 'Dutch-Netherlands' 
+ },
+
+{ 
+ 'code': 'no-NO', 
+'name': 'Norwegian-Norway' 
+ },
+
+{ 
+ 'code': 'pl-PL', 
+'name': 'Polish-Poland' 
+ },
+
+{ 
+ 'code': 'pt-BR', 
+'name': 'Portuguese-Brazil' 
+ },
+
+{ 
+ 'code': 'pt-PT', 
+'name': 'Portuguese-Portugal' 
+ },
+
+{ 
+ 'code': 'ro-RO', 
+'name': 'Romanian-Romania' 
+ },
+
+{ 
+ 'code': 'ru-RU', 
+'name': 'Russian-Russian Federation' 
+ },
+
+{ 
+ 'code': 'sk-SK', 
+'name': 'Slovak-Slovakia' 
+ },
+
+{ 
+ 'code': 'sv-SE', 
+'name': 'Swedish-Sweden' 
+ },
+
+{ 
+ 'code': 'th-TH', 
+'name': 'Thai-Thailand' 
+ },
+
+{ 
+ 'code': 'tr-TR', 
+'name': 'Turkish-Turkey' 
+ },
+
+{ 
+ 'code': 'zh-CN', 
+'name': 'Chinese-China' 
+ },
+
+{ 
+ 'code': 'zh-HK', 
+'name': 'Chinese-Hong Kong' 
+ },
+
+{ 
+ 'code': 'zh-TW', 
+'name': 'Chinese-Taiwan' 
+ },
+];
+ export default langs;
