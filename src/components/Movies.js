53c5414bfad4ed0dbc64ce1ec8c0557b4dcf6795
Jacob Greenway <jagreenway12@gmail.com>
2019-01-27 04:37:58 -0600

    merged james code

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,9 +1,63 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import url from 'url';
+
+import './Movies.css';
+
+const populateMovies = (callback) => {
+  axios.get(url.resolve(process.env.REACT_APP_MEDIA_SERVER, '/movies'))
+  .then(res => {
+    console.log(res.data);
+    callback(res.data)
+  })
+  .catch(err =>{
+    console.log(err.response);
+    callback(null, err)
+  });
+}
 
 export default class Movies extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movies: []
+    };
+  }
+
+  componentDidMount() {
+    populateMovies((res) => {
+      this.setState({
+        movies: res
+      });
+    });
+  }
+
+  render() {
+
+    let movies = this.state.movies.map((movie) => (
+      <li key={movie.id}><Movie title={movie.title} desc={movie.desc} thumbnail={movie.thumbnail}></Movie>
+      </li>
+    ));
+
+    return (
+      <div>
+        <ul>
+          {movies}
+        </ul>
+      </div>
+    );
+  }
+}
+
+class Movie extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <img src={this.props.thumbnail} />
+        <h3>{this.props.title}</h3>
+        <p>{this.props.desc}</p>
+      </div>
     );
   }
 }
